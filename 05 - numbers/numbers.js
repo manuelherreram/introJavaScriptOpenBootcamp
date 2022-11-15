f0da20442a030f79c996c44d574a3c059f8f99f4
Manuel Herrera <manuel.herrera.m@gmail.com>
2022-11-14 23:05:18 -0300

    Ejercicio numbers

---
+++ 05 - numbers/numbers.js
@@ -0,0 +1,6 @@
+let alturaCentimetros = 167;
+let alturaMetros = 1.67;
+let pesoKilos = 84.5;
+let alturaMetrosRound = Math.ceil(alturaMetros);
+let pesoKilosRound = Math.floor(pesoKilos);
+let seranIguales = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
